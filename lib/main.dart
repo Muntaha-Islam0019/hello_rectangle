97ef3b71052e968b31a6af1f1fef98528ea87552
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-11-10 16:00:13 +0600

    Added helloRectange function and MaterialApp and Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
+// Top level function to keep the code clean by putting body widgets.
+Widget helloRectangle() {
+  return Container(
+    color: Colors.cyanAccent,
+  );
+}
+
 void main() {
+  // runApp takes widgets as arguments.
   runApp(
-    Container(
-      color: Colors.cyanAccent,
+    // Material app contains most of the features we want in an app; for instance: title, home, theme, built-in text directionality, back button press etc.
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Scaffold offers drawers, AppBars, bottom navigation, tabs and floating action buttons.
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Rectangle"),
+        ),
+        body: helloRectangle(),
+      ),
     ),
   );
 }
