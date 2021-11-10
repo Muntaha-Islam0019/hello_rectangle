e9a50f41e1c6135623c34574a0cfcecfc3d75fb2
Muntaha-Islam0019 <muntaha.islam0019@gmail.com>
2021-11-10 16:36:02 +0600

    Centralised the rectangle and text

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 // Old: Top level function to keep the code clean by putting body widgets.
 // Inheriting from StatelessWidget for added functionality.
 class HelloRectangle extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.cyanAccent,
+    return Center(
+      child: Container(
+        color: Colors.cyanAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text("Hello!",
+          style: TextStyle(fontSize: 40.0),),
+        ),
+      ),
     );
   }
 }
